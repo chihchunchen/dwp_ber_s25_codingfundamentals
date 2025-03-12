b37f70d59cfcce03640d9706689a7633b3474bbb
chihchunchen <chihchunc@gmail.com>
2025-03-12 15:42:32 +0000

    Adding solutions to first two questions

---
+++ homework/lesson-1/main.py
@@ -0,0 +1,16 @@
+#1. Print `Hello World!` by using the `print()` 
+print("#1")
+print("Hello World!")
+
+#2. Print `My name is <INSERT YOUR NAME HERE>`  
+print("#2")
+print("My name is Chih-Chun")
+name = "Chih-Chun"
+print("With placeholder:")
+print(f"My name is {name}")
+
+#3. Save your changes on codespaces (It's ok if you forgot how to do it, then, go the Class material, Slide 57 to 61)
+
+
+#4. Stop running codespaces (it's very important to not consume all your free monthly credits!)
+#5. Share `lesson-1/main.py` link into L1 Assignment section on Google Classroom
